42c2cae53df888442815da6b4380579e69beb2b7
Josema996 <101608821+Josema996@users.noreply.github.com>
2023-08-09 20:24:50 -0300

    cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,111 @@
-import { useState } from "react"
-import {RiMenu3Fill, RiUser3Line, RiAddLine, RiPieChartLine, RiCloseLine,RiArrowDownSLine} from "react-icons/ri"
-import Cart from "./components/shared/Cart";
+import { useState } from "react";
+import {
+  RiMenu3Fill,
+  RiUser3Line,
+  RiAddLine,
+  RiPieChartLine,
+  RiCloseLine,
+  RiArrowDownSLine,
+} from "react-icons/ri";
+// Components
+import Sidebar from "./components/shared/Sidebar";
+import Car from "./components/shared/Car";
 import Header from "./components/shared/Header";
-import Sidebar from "./components/shared/Sidebar"
 import Card from "./components/shared/Card";
-function App() {
 
+function App() {
   const [showMenu, setShowMenu] = useState(false);
   const [showOrder, setShowOrder] = useState(false);
 
   const toggleMenu = () => {
     setShowMenu(!showMenu);
     setShowOrder(false);
-  }
+  };
 
   const toggleOrders = () => {
     setShowOrder(!showOrder);
     setShowMenu(false);
-  }
+  };
 
   return (
     <div className="bg-[#262837] w-full min-h-screen">
-    <Sidebar showMenu={showMenu} />
-    <Cart showOrder={showOrder} setShowOrder={setShowOrder} />
-    {/*Menu Movil*/}
+      <Sidebar showMenu={showMenu} />
+      <Car showOrder={showOrder} setShowOrder={setShowOrder} />
+      {/* Menu movil */}
       <nav className="bg-[#1F1D2B] lg:hidden fixed w-full bottom-0 left-0 text-3xl text-gray-400 py-2 px-8 flex items-center justify-between rounded-tl-xl rounded-tr-xl">
-        <button className="p-2"><RiUser3Line/></button>
-        <button className="p-2"><RiAddLine/></button>
-        <button className="p-2" onClick={toggleOrders}><RiPieChartLine/></button>
-        <button className="text-white p-2" onClick={toggleMenu}>{showMenu ? <RiCloseLine/> : <RiMenu3Fill />}</button>
+        <button className="p-2">
+          <RiUser3Line />
+        </button>
+        <button className="p-2">
+          <RiAddLine />
+        </button>
+        <button onClick={toggleOrders} className="p-2">
+          <RiPieChartLine />
+        </button>
+        <button onClick={toggleMenu} className="text-white p-2">
+          {showMenu ? <RiCloseLine /> : <RiMenu3Fill />}
+        </button>
       </nav>
       <main className="lg:pl-32 lg:pr-96 pb-20">
         <div className="md:p-8 p-4">
           {/* Header */}
           <Header />
-           {/*Title Content */}
-           <div className="flex items-center justify-between mb-16">
-              <h2 className="text-xl text-gray-300">Choose Dishes</h2>
-              <button className="flex items-center gap-4 text-gray-300 bg-[#1F1D2B] py-2 px-4 rounded-lg"><RiArrowDownSLine />Dine In</button>
-            </div>
-
-            {/*Content*/}
-            <div className="p-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-16">
-              {/*Card1*/}
-              <Card img="comida.png" description="Speacy seasoned seafood nodles" price="2.29" inventory="20" />
-               {/*Card2*/}
-              <Card img="comida2.png" description="Curry with stir-fried rice" price="1.29" inventory="40" />
-                {/*Card3*/}
-              <Card img="comida3.png" description="Margarita pizza" price="5.29" inventory="30" />
-               {/*Card4*/}
-              <Card img="comida4.png" description="Mixed salad" price="3.29" inventory="15" />
-                {/*Card5*/}
-              <Card img="comida5.png" description="Speacy seasoned seafood nodles" price="2.29" inventory="20" />
-               {/*Card6*/}
-              <Card img="comida6.png" description="Speacy seasoned seafood nodles" price="2.29" inventory="20" />
-             
-            </div>
+          {/* Title content */}
+          <div className="flex items-center justify-between mb-16">
+            <h2 className="text-xl text-gray-300">Choose Dishes</h2>
+            <button className="flex items-center gap-4 text-gray-300 bg-[#1F1D2B] py-2 px-4 rounded-lg">
+              <RiArrowDownSLine /> Dine in
+            </button>
+          </div>
+          {/* Content */}
+          <div className="p-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-16">
+            {/* Card */}
+            <Card
+              img="comida.png"
+              description="Speacy seasoned seafood nodles"
+              price="2.29"
+              inventory="25"
+            />
+            {/* Card */}
+            <Card
+              img="comida2.png"
+              description="Stir-fried chicken with rice"
+              price="3.19"
+              inventory="20"
+            />
+            {/* Card */}
+            <Card
+              img="comida3.png"
+              description="Margarita pizza"
+              price="5.00"
+              inventory="13"
+            />
+            {/* Card */}
+            <Card
+              img="comida4.png"
+              description="Mixed salad"
+              price="1.29"
+              inventory="15"
+            />
+            {/* Card */}
+            <Card
+              img="comida5.png"
+              description="Chickpea puree"
+              price="3.00"
+              inventory="10"
+            />
+            {/* Card */}
+            <Card
+              img="comida6.png"
+              description="Children's menu"
+              price="7.29"
+              inventory="50"
+            />
+          </div>
         </div>
       </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
